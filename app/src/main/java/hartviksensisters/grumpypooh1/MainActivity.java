9c02ff00b3c7933cb8e928d6abb79b3cc8fef84d
RACHEL HARTVIKSEN <rhartviksen@gmail.com>
2015-04-05 12:06:11 -0400

    Adding Goochi-gooing button

--- app/src/main/java/hartviksensisters/grumpypooh1/MainActivity.java
+++ app/src/main/java/hartviksensisters/grumpypooh1/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,19 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize UI Elements
+        Button beFriendButton = (Button) findViewById(R.id.beMyFriendButton);
+        beFriendButton.setText(R.string.be_my_friend);
+
+        // Link UI elements to code
+        beFriendButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextView theResponse = (TextView) findViewById(R.id.responseText);
+                theResponse.setText(theResponse.getText() + " goochi goo!");
+            }
+
+        });
     }
 
 
